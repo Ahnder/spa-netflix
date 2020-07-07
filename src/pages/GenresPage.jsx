01081e749c61ad2ffec90c3bf87d326c4887b2a8
Ahnder <skguss1@gmail.com>
2020-07-07 17:48:06 +0900

    First Setting Pages - Main, Genres, MyList, Apply react-router-dom App Setting

---
+++ src/pages/GenresPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GenresPage = () => {
+  return <div>장르별영화</div>;
+};
+
+export default GenresPage;
---
+++ src/pages/MyListPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyListPage = () => {
+  return <div>마이리스트</div>;
+};
+
+export default MyListPage;
